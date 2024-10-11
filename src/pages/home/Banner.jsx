d97cf4fb05c03cab7e4b95acc80da417fed725c5
baquy111103 <145130742+baquy111103@users.noreply.github.com>
2024-10-11 19:46:58 +0700

    FE-1

---
+++ src/pages/home/Banner.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import bannerImg from "../../assets/header.png"
+
+const Banner = () => {
+    return (
+        <div className='section__container header__container'>
+            <div className='header__content z-30'>
+                <h4 className='uppercase'>UP TO 20% DISCOUNT ON</h4>
+                <h1>New Technology</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sequi, optio eius, neque consequatur dicta quibusdam commodi omnis quod corporis praesentium velit placeat assumenda voluptate rerum consequuntur aspernatur quia perferendis.</p>
+                <button className='btn'><Link to='/shop'>EXPLORE NOW</Link></button>
+            </div>
+            <div className='header__image'>
+                <img src={bannerImg} alt="banner image" />
+            </div>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/pages/shop/TrendingProducts.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import ProductCards from './ProductCards'
+
+import products from "../../data/products.json"
+
+const TrendingProducts = () => {
+    const [visibleProducts, setVisibleProducts] = useState(8);
+
+    const loadMoreProducts = () => {
+        setVisibleProducts(prevCount => prevCount + 4)
+    }
+    return (
+        <section className='section__container product__container'>
+            <h2 className='section__header'>Trending Products</h2>
+            <p className='section__subheader mb-12'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate eveniet ipsa assumenda deleniti voluptatem quod? Veniam harum itaque mollitia id reprehenderit? Neque sint, magni commodi ut ipsa nam eius esse.</p>
+
+            {/* Products card */}
+            <div className="mt-12">
+                <ProductCards products={products} />
+            </div>
+
+
+        </section>
+    )
+}
+
+export default TrendingProducts
