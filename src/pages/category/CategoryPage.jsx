a04003ced40cb1db62afe09898fedea4b9ebb97b
baquy111103 <145130742+baquy111103@users.noreply.github.com>
2024-10-15 16:01:52 +0700

    done search

---
+++ src/pages/category/CategoryPage.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import products from "../../data/products.json"
+import ProductCards from '../shop/ProductCards';
+
+const CategoryPage = () => {
+    const { categoryName } = useParams();
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(() => {
+        const filtered = products.filter((product) => product.category === categoryName.toLowerCase());
+
+        setFilteredProducts(filtered);
+    }, [categoryName])
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    })
+
+    return (
+        <>
+            <section className='section__container bg-primary-light'>
+                <h2 className='section__header capitalize'>{categoryName}</h2>
+                <p className='section__subheader'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus dignissimos minus eaque adipisci dolores. Odio temporibus accusamus tempora est quis, culpa cumque cum nihil pariatur repudiandae totam et voluptates autem!</p>
+            </section>
+
+            {/* Products Card */}
+            <div className='section__container'>
+                <ProductCards products={filteredProducts} />
+            </div>
+        </>
+    )
+}
+
+export default CategoryPage
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,20 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
+import CategoryPage from "../pages/category/CategoryPage";
+import Search from "../pages/search/Search";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
-            {
-                path: "/",
-                element: <Home />
-            },
-            {
-                path: "/about",
-                element: <div>About Page</div>
-            }
+            { path: "/", element: <Home /> },
+            { path: "/categories/:categoryName", element: <CategoryPage /> },
+            { path: "/search", element: <Search /> }
         ]
     },
 ]);
